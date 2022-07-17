4b4fb59dbb0c1e4cf68b87b13028f12c0dd6e417
Philipe <philipe-leandro@hotmail.com>
2022-07-17 17:24:44 -0300

    Create config file to sidekiq

---
+++ sidekiq/config.rb
@@ -0,0 +1,11 @@
+require 'sidekiq'
+require_relative '../sidekiq_worker/worker.rb'
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://redis:6379/0'}
+end
+
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://redis:6379/0'}
+end
