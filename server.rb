8588866c84ac6fa4769a14a5df87945c025ca024
Philipe <philipe-leandro@hotmail.com>
2022-07-18 16:54:28 -0300

    Create endpoint to show details

--- server.rb
+++ server.rb
@@ -2,8 +2,10 @@ require 'sinatra'
 require 'rack/handler/puma'
 require 'csv'
 require 'pg'
-require_relative'./helper/list_csv_data'
-require_relative  './helper/data_csv'
+require_relative './helper/list_csv_data'
+require_relative './helper/data_csv'
+require_relative './helper/database_connection'
+require_relative './helper/configure_csv'
 require 'sidekiq'
 require 'sidekiq/web'
 require 'redis'
@@ -20,6 +22,18 @@ end
 post '/import' do
   MyWorker.perform_async(request.body.read)
   puts 'Seus dados estão sendo importados'
+end
+
+get '/tests/:token' do
+  tests = Database.connect.exec_params("select exam_type, exam_type_limit, exam_result from patient where exam_result_token = '#{params["token"]}'").to_a
+  doctor = Database.connect.exec_params("select distinct crm, crm_state, doctor_name from patient where exam_result_token = '#{params["token"]}'")
+  patient = Database.connect.exec_params("select distinct exam_result_token, exam_date, cpf, name, email, birthdate from patient where exam_result_token = '#{params["token"]}'")
+
+  result = patient.first
+  result = result.merge('doctor' => doctor.first)
+  result = result.merge('tests' => tests)
+  
+  result.to_json
 end
 
 Rack::Handler::Puma.run(
