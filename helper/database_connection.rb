9d909705d93d9c35b229bce2a764468307d83b73
Philipe <philipe-leandro@hotmail.com>
2022-07-18 16:53:40 -0300

    Fix host ip

--- helper/database_connection.rb
+++ helper/database_connection.rb
@@ -2,6 +2,6 @@ require 'pg'
 
 class Database
   def self.connect
-    PG.connect(dbname: 'postgres', host: '172.19.0.2', port: 5432, user: 'postgres', password: 'password')
+    PG.connect(dbname: 'postgres', host: '172.19.0.3', port: 5432, user: 'postgres', password: 'password')
   end
 end
