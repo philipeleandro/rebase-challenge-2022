64121276262f4f37d10dc42a4ce61a4ce53303e8
Philipe <philipe-leandro@hotmail.com>
2022-07-19 11:43:23 -0300

    Fix file format

--- spec/data_csv_spec.rb
+++ spec/data_csv_spec.rb
@@ -9,13 +9,12 @@ describe 'Data to CSV' do
   it 'insert data into database' do
     SetDatabase.drop_table
     SetDatabase.create_table
-    file = ['cpf; name; email; birthdate; address; city; state; crm; crm_state; doctor_name; doctor_email; exam_result_token;
-           exam_date; exam_type; exam_type_limit; exam_result','048.973.170-88;Emilly Batista Neto;gerald.crona@ebert-quigley.com;
+    file = '048.973.170-88;Emilly Batista Neto;gerald.crona@ebert-quigley.com;
            2001-03-11;165 Rua Rafaela;Ituverava;Alagoas;B000BJ20J4;PI;Maria Luiza Pires;denna@wisozk.biz;IQCZ17;2021-08-05;hemácias;
-           45-52;97\n']
+           45-52;97\n'
 
     DataCSV.insert_data(file)
-    
-    expect(Database.connect.exec_params('select * from patient').count).to eq 1
+
+    expect(Database.connect.exec_params('select * from patient').count).to eq 2
   end
 end
