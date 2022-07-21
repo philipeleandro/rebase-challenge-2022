f94f0cea60e9b4d7b111ad71ecd0d1f61d6bb170
Philipe <philipe-leandro@hotmail.com>
2022-07-21 10:09:20 -0300

    Add message to importing from csv file

--- helper/import_from_csv.rb
+++ helper/import_from_csv.rb
@@ -2,6 +2,15 @@
 
 require_relative 'set_database'
 
+puts 'Starting process'
+puts '-----------------------'
+puts 'Droping existing table'
+puts '-----------------------'
 SetDatabase.drop_table
+puts 'Creating table'
+puts '-----------------------'
 SetDatabase.create_table
+puts 'Importing CSV data'
+puts '-----------------------'
 SetDatabase.data_to_database
+puts 'Done'
